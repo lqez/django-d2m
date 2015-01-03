bd49b4725e1352907955a95576fb35f856868f0b
Park Hyunwoo <ez@smartstudy.co.kr>
2015-01-03 10:44:35 +0900

    Add fixture for testing

--- django_d2m/testapp/models.py
+++ django_d2m/testapp/models.py
@@ -1,18 +1,34 @@
 from django.db import models
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Article(models.Model):
-    subject = models.CharField(max_length=100)
-    body = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     read = models.IntegerField(default=0)
 
     author = models.ForeignKey(Author)
     tags = models.ManyToManyField(Tag)
+
+    def __unicode__(self):
+        return self.title
