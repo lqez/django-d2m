abed8a7c3ca6dabbadb0d36552bbd87d74092e1f
Park Hyunwoo <ez@smartstudy.co.kr>
2015-04-30 18:45:38 +0900

    Fix for python3

--- django_d2m/__init__.py
+++ django_d2m/__init__.py
@@ -2,6 +2,6 @@ version_info = (0, 1, 4)
 
 __version__ = VERSION = '.'.join(map(str, version_info))
 
-from d2m import queryset_to_model, list_to_model, dict_to_model
+from .d2m import queryset_to_model, list_to_model, dict_to_model
 
 __all__ = ['queryset_to_model', 'list_to_model', 'dict_to_model']
