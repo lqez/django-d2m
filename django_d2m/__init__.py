c01b74f82f67dbcf07c0609e71c3ecfb5b485d1e
Park Hyunwoo <ez@smartstudy.co.kr>
2015-04-30 18:49:54 +0900

    Bump version and fix for PEP440

--- django_d2m/__init__.py
+++ django_d2m/__init__.py
@@ -1,6 +1,4 @@
-version_info = (0, 1, 4)
-
-__version__ = VERSION = '.'.join(map(str, version_info))
+__version__ = VERSION = '0.1.4'
 
 from .d2m import queryset_to_model, list_to_model, dict_to_model
 
